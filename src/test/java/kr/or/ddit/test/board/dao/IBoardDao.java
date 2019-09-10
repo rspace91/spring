d5465d596665720b4ff4bf64537da4e7ff511ec9
Ryu <rspace91@gamil.com>
2019-09-10 13:40:35 +0900

    spring 프로젝트(스프링컨테이너생성)

---
+++ src/test/java/kr/or/ddit/test/board/dao/IBoardDao.java
@@ -0,0 +1,11 @@
+package kr.or.ddit.test.board.dao;
+
+public interface IBoardDao {
+	/**
+	 * Method : getBoardList
+	 * 작성자 : PC-12
+	 * 변경이력 :
+	 * Method 설명 : 게시판 리스트를 조회한다.
+	 */
+	void getBoardList();
+}
