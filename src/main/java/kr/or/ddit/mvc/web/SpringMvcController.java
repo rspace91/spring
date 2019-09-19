350b1b733aac48f575106258180d81e7ca7ac380
Ryu <rspace91@gamil.com>
2019-09-19 16:17:31 +0900

    "spring mvc  . ModelAndView  . viewResolver    . beanNameViewResolver (jsonView)    . ajax  . customView   . fileDownloadView"

--- src/main/java/kr/or/ddit/mvc/web/SpringMvcController.java
+++ src/main/java/kr/or/ddit/mvc/web/SpringMvcController.java
@@ -27,7 +27,9 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import kr.or.ddit.exception.NoFileException;
 import kr.or.ddit.mvc.model.Main;
@@ -42,6 +44,8 @@ import kr.or.ddit.util.model.FileInfo;
 public class SpringMvcController {
 	private static final Logger logger = LoggerFactory.getLogger(SpringMvcController.class);
 	
+	@javax.annotation.Resource(name="jsonView")
+	private View jsonView;
 	//@RequestMapping이 붙은 메소드가 실행되기전에 @ModelAttribute 메소드가 먼저 실행되고
 	//해당 메소드가 리턴하는 값을 Model 객체에 자동으로 넣어준다
 	//해당 컨트롤러에 대해서만 처리
@@ -217,5 +221,34 @@ public class SpringMvcController {
 		return "mvc/view";
 	}
 	
+	@RequestMapping("jsonView")
+	public String jsonView(Model model) {
+		List<String> rangers = new ArrayList<String>();
+		rangers.add("brown");
+		rangers.add("cony");
+		rangers.add("sally");
+		
+		model.addAttribute("rangers", rangers);
+		
+		return "jsonView";
+	}
 	
+	@RequestMapping("jsonView2")
+	public View jsonView2(Model model) {
+		List<String> rangers = new ArrayList<String>();
+		rangers.add("brown");
+		rangers.add("cony");
+		rangers.add("sally");
+		
+		model.addAttribute("rangers", rangers);
+		
+		return jsonView;
+//		return new MappingJackson2JsonView();
+	}
+	
+	@RequestMapping("fileDownloadView")
+	public String fileDownloadView(String pictureName, Model model) {
+		model.addAttribute("pictureName", pictureName);
+		return "fileDownloadView";
+	}
 }
